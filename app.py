8ce74dacf5b830e9f4fb6d2e28ca3f92279f7ff8
swapn <swapnilbharte00@gmail.com>
2026-02-11 15:28:43 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = 'c42a15b665b43edc082cd06ed9b83565'
+
+user_input = input("Enter City: ")
+
+weather_data =requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+  print("No City Found ")
+else:
+  weather = weather_data.json()['weather'][0]['main']
+  temp = round(weather_data.json()['main']['temp'])
+
+  print(f"The Weather in {user_input} is: {weather}")
+  print(f"The temperture in {user_input} is: {temp}")
+
+
